1299ba4338934fc8baed93ca58d33ebe9e8377af
Baturalp KIZILTAN <baturalp.kiziltan@outlook.com>
2023-03-30 13:08:41 +0300

    Build CallToAction (CTA) component

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -1,5 +1,16 @@
+import Link from 'next/link';
+import styles from './CallToAction.module.css';
+
 function CallToAction() {
-  return <div>CallToAction</div>;
+  return (
+    <section id={styles.root}>
+      <h2>{"🔥 Aidiyet Bülten'in 2. Sayısı Çıktı !"}</h2>
+      <div>
+        <Link href={'/yayinlarimiz'}>Yayınlarımız</Link>
+        <Link href={'/#'}>Bülten 2. Sayı</Link>
+      </div>
+    </section>
+  );
 }
 
 export default CallToAction;
